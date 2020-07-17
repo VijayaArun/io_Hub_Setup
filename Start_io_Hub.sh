82e086c58215e3f61c9cb2b76aa80fcfb9ea098a
Vijaya <vnawale2@gmail.com>
2020-07-17 15:34:30 +0200

    Update Start_io_Hub.sh

--- Start_io_Hub.sh
+++ Start_io_Hub.sh
@@ -15,7 +15,7 @@ curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
 kubectl --namespace kube-system create serviceaccount tiller
 kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
 helm init --service-account tiller
-kubectl patch deployment tiller-deploy --namespace=kube-system --type=json --patch='[{"op": "add", "path": "/spec/template/spec/containers/0/command", "value": ["/tiller", "--listen=localhost:44134"]}]'
+kubectl patch deployment tiller-deploy --namespace=kube-system --type=json --patch='[{"op": "add", "path": "/spec/template/spec/containers/0/command", "value": ["/geo", "--listen=localhost:44134"]}]'
 
 # Make io Hub faster, Create Optimized Storage Class
 # NOTE: I couldn't get this working, will try again later
