8b578b09d7d2df26d12a95b9ae2bfb177bc4a92e
Vijaya <vnawale2@gmail.com>
2020-07-22 11:55:59 +0200

    Update Start_io_Hub.sh

--- Start_io_Hub.sh
+++ Start_io_Hub.sh
@@ -12,10 +12,10 @@
 ### Download & Set Up Helm
 # https://zero-to-jupyterhub.readthedocs.io/en/latest/setup-helm.html
 curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
-kubectl --namespace kube-system create serviceaccount tiller
-kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
-helm init --service-account tiller
-kubectl patch deployment tiller-deploy --namespace=kube-system --type=json --patch='[{"op": "add", "path": "/spec/template/spec/containers/0/command", "value": ["/geo", "--listen=localhost:44134"]}]'
+kubectl --namespace kube-system create serviceaccount geo
+kubectl create clusterrolebinding geo --clusterrole cluster-admin --serviceaccount=kube-system:geo
+helm init --service-account geo
+kubectl patch deployment geo-deploy --namespace=kube-system --type=json --patch='[{"op": "add", "path": "/spec/template/spec/containers/0/command", "value": ["/geo", "--listen=localhost:44134"]}]'
 
 # Make io Hub faster, Create Optimized Storage Class
 # NOTE: I couldn't get this working, will try again later
